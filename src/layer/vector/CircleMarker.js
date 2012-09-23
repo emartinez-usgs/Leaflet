1820505de042d772dc52bb9ba092cc0d24e1a59b
Fleur Kelpin <fleur@kelpin.nl>
2012-09-23 15:50:51 +0200

    radius is part of CircleMarker style so should be changeable in using setStyle();

--- src/layer/vector/CircleMarker.js
+++ src/layer/vector/CircleMarker.js
@@ -16,6 +16,11 @@ L.CircleMarker = L.Circle.extend({
 	projectLatlngs: function () {
 		this._point = this._map.latLngToLayerPoint(this._latlng);
 	},
+	
+	_updateStyle : function () {
+		L.Circle.prototype._updateStyle.call(this);
+		this.setRadius(this.options.radius);
+	},
 
 	_updateStyle : function () {
 		L.Circle.prototype._updateStyle.call(this);
