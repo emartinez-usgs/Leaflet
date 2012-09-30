f6315e9a52a1731f33bfecfe6f24299aac00ad8e
danzel <danzel@localhost.geek.nz>
2012-10-01 11:00:19 +1300

    Make DomEvent.DoubleTap support MSPointer* events. Had to track touch count manually and clone the object so we can change the type.

--- src/dom/DomEvent.DoubleTap.js
+++ src/dom/DomEvent.DoubleTap.js
@@ -1,8 +1,4 @@
-/*
- * Extends the event handling code with double tap support for mobile browsers.
- */
-
-L.extend(L.DomEvent, {
+L.Util.extend(L.DomEvent, {
 
 	_touchstart: L.Browser.msTouch ? 'MSPointerDown' : 'touchstart',
 	_touchend: L.Browser.msTouch ? 'MSPointerUp' : 'touchend',
@@ -10,24 +6,17 @@ L.extend(L.DomEvent, {
 	// inspired by Zepto touch code by Thomas Fuchs
 	addDoubleTapListener: function (obj, handler, id) {
 		var last,
-		    doubleTap = false,
-		    delay = 250,
-		    touch,
-		    pre = '_leaflet_',
-		    touchstart = this._touchstart,
-		    touchend = this._touchend,
-		    trackedTouches = [];
+			doubleTap = false,
+			delay = 250,
+			touch,
+			pre = '_leaflet_',
+			touchstart = this._touchstart,
+			touchend = this._touchend,
+			touchCount = 0;
 
 		function onTouchStart(e) {
-			var count;
-
-			if (L.Browser.msTouch) {
-				trackedTouches.push(e.pointerId);
-				count = trackedTouches.length;
-			} else {
-				count = e.touches.length;
-			}
-			if (count > 1) {
+			touchCount++;
+			if (touchCount > 1) {
 				return;
 			}
 
@@ -40,29 +29,15 @@ L.extend(L.DomEvent, {
 		}
 
 		function onTouchEnd(e) {
-			if (L.Browser.msTouch) {
-				var idx = trackedTouches.indexOf(e.pointerId);
-				if (idx === -1) {
-					return;
-				}
-				trackedTouches.splice(idx, 1);
-			}
-
+			touchCount--;
 			if (doubleTap) {
 				if (L.Browser.msTouch) {
-					// work around .type being readonly with MSPointer* events
-					var newTouch = { },
-						prop;
-
-					// jshint forin:false
+					//Work around .type being readonly with MSPointer* events
+					var newTouch = { };
 					for (var i in touch) {
-						prop = touch[i];
-						if (typeof prop === 'function') {
-							newTouch[i] = prop.bind(touch);
-						} else {
-							newTouch[i] = prop;
-						}
+						newTouch[i] = touch[i];
 					}
+					delete newTouch.preventDefault;
 					touch = newTouch;
 				}
 				touch.type = 'dblclick';
@@ -89,15 +64,8 @@ L.extend(L.DomEvent, {
 
 	removeDoubleTapListener: function (obj, id) {
 		var pre = '_leaflet_';
-
-		obj.removeEventListener(this._touchstart, obj[pre + this._touchstart + id], false);
-		(L.Browser.msTouch ? document.documentElement : obj).removeEventListener(
-		        this._touchend, obj[pre + this._touchend + id], false);
-
-		if (L.Browser.msTouch) {
-			document.documentElement.removeEventListener('MSPointerCancel', obj[pre + this._touchend + id], false);
-		}
-
+		obj.removeEventListener(obj, obj[pre + this._touchstart + id], false);
+		obj.removeEventListener(obj, obj[pre + this._touchend + id], false);
 		return this;
 	}
 });
