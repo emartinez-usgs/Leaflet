c66d6ce3b931f431416d24bd252ef82735bd3323
Alexander Parshin <parshin.alexander@gmail.com>
2012-10-10 03:05:12 +0400

    Allow HTML in Layer Names (issue #1009)

--- src/control/Control.Layers.js
+++ src/control/Control.Layers.js
@@ -187,7 +187,7 @@ L.Control.Layers = L.Control.extend({
 		if (obj.overlay) {
 			input = document.createElement('input');
 			input.type = 'checkbox';
-			input.className = 'leaflet-control-layers-selector';
+            input.className = 'leaflet-control-layers-selector';
 			input.defaultChecked = checked;
 		} else {
 			input = this._createRadioElement('leaflet-base-layers', checked);
@@ -197,8 +197,8 @@ L.Control.Layers = L.Control.extend({
 
 		L.DomEvent.on(input, 'click', this._onInputClick, this);
 
-		var name = document.createElement('span');
-		name.innerHTML = ' ' + obj.name;
+        var name = document.createElement('span');
+        name.innerHTML = ' ' + obj.name;
 
 		label.appendChild(input);
 		label.appendChild(name);
